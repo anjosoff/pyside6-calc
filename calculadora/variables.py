0c238dde5b990007354991cc254f88901eae734b
guilherme.anjos <anjos.guilherme.estagiario@tre-rn.jus.br>
2023-04-19 16:56:39 -0300

    change application icon

--- calculadora/variables.py
+++ calculadora/variables.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 ROOT_DIR=Path(__file__).parent
 FILES_DIR=ROOT_DIR/'files'
-WINDOW_ICON_PATH=FILES_DIR/'calculator.png'
+WINDOW_ICON_PATH=FILES_DIR/'favicon.png'
 
 
 # Colors
